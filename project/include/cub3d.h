5724b448715ef29f8b0f69cadd1ab5b64341863e
yunemak <yeak558@gmail.com>
2026-06-26 20:07:29 +0300

    Run first program

--- project/include/cub3d.h
+++ project/include/cub3d.h
@@ -1,8 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yuak <yuak@student.42istanbul.com.tr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/26 20:00:21 by yuak              #+#    #+#             */
+/*   Updated: 2026/06/26 20:00:21 by yuak             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef CUB3D_H
 # define CUB3D_H
 
 #include <stdio.h>
 #include "../libft/libft.h"
+#include "../minilibx-linux/mlx.h"
 #include "byaprak.h"
 
 #endif
--- project/src/main.c
+++ project/src/main.c
@@ -6,7 +6,7 @@
 /*   By: yuak <yuak@student.42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/15 20:37:38 by yuak              #+#    #+#             */
-/*   Updated: 2026/06/26 16:07:55 by yuak             ###   ########.fr       */
+/*   Updated: 2026/06/26 20:01:10 by yuak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,21 @@
 
 int	main(void)
 {
-	if (!ft_isdigit('5'))
+	void	*conn_ptr;
+	void	*win_ptr;
+
+	conn_ptr = mlx_init();
+	if (!conn_ptr)
+	{
+		printf("Connection with the X server cannot be esthabilished!\n");
 		return (1);
-	printf("Hello world!\n");
+	}
+	win_ptr = mlx_new_window(conn_ptr, 800, 600, "The First Window");
+	if (!win_ptr)
+	{
+		printf("Window cannot be created!\n");
+		return (2);
+	}
+	mlx_loop(conn_ptr);
 	return (0);
 }
