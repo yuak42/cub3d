59aa20f974393018dd4e749e0b4a9b818c37ca1b
yunemak <yeak558@gmail.com>
2026-07-27 15:19:01 +0300

    test

--- project/src/tests/test_parser.c
+++ project/src/tests/test_parser.c
@@ -1,30 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   test_parser.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yuak <yuak@student.42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/06/15 20:37:38 by yuak              #+#    #+#             */
-/*   Updated: 2026/07/27 15:13:03 by yuak             ###   ########.fr       */
+/*   Created: 2026/07/27 15:13:42 by yuak              #+#    #+#             */
+/*   Updated: 2026/07/27 15:13:46 by yuak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int	main(int ac, char **ar)
+int	test_parser(t_game *game)
 {
-	(void) ac;
-	(void) ar;
-	t_game	*game;
 
-	// check_input(ac, ar); 
 
-	game = parse(ac, ar);
-	test_parser(game);
-	// if (!game)
-	// 	return (1);
-	// render(game);
-	// free everyting
+
+
 	return (0);
 }
