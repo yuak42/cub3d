b3368606750d13eccac450ea0924008716e5a48e
yunemak <yeak558@gmail.com>
2026-08-06 12:01:28 +0300

    fix: data.h t_game

--- project/src/main.c
+++ project/src/main.c
@@ -6,7 +6,7 @@
 /*   By: yuak <yuak@student.42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/15 20:37:38 by yuak              #+#    #+#             */
-/*   Updated: 2026/08/06 11:56:34 by yuak             ###   ########.fr       */
+/*   Updated: 2026/08/06 12:00:15 by yuak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ int	main(int ac, char **ar)
 	game = parse(ac, ar);
 	if (!game)
 		return (1);
-	// test_parser(game);
 	// if (!game)
 	// 	return (1);
 	// render(game);
