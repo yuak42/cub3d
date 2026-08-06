35ffbdcf1a5daf66dfd3142f6ec149f7ee42d846
yunemak <yeak558@gmail.com>
2026-08-06 13:55:53 +0300

    feat: Add textures

--- project/src/parser/create_game.c
+++ project/src/parser/create_game.c
@@ -6,7 +6,7 @@
 /*   By: yuak <yuak@student.42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/27 22:26:15 by yuak              #+#    #+#             */
-/*   Updated: 2026/08/06 12:41:24 by yuak             ###   ########.fr       */
+/*   Updated: 2026/08/06 13:43:46 by yuak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,15 @@
 
 t_game	*create_game(char *cub)
 {
-	t_map	*map;
 	t_game	*game;
 
 	game = (t_game *) ft_calloc(1, sizeof(t_game));
 	if (!game)
 		return (NULL);
-	map = create_map(cub);
-	if (!map->grid)
+	game->map = create_map(cub);
+	if (!game->map->grid)
 		return (NULL);
-	game->map = map;
 	if (get_texture_paths(cub, game) != 0)
 		return (NULL);
 	return (game);
 }
-
-
-
