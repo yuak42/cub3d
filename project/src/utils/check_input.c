b2e0bc8a464864d1239d4a440fda2c325f4ecb6d
yunemak <yeak558@gmail.com>
2026-08-08 21:12:06 +0300

    Start error check

--- project/src/utils/check_input.c
+++ project/src/utils/check_input.c
@@ -6,7 +6,7 @@
 /*   By: yuak <yuak@student.42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/06 20:23:55 by yuak              #+#    #+#             */
-/*   Updated: 2026/08/06 20:30:33 by yuak             ###   ########.fr       */
+/*   Updated: 2026/08/08 21:11:55 by yuak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,44 @@ int check_input(int ac, char **av)
 
 int check_file(char *cub)
 {
-	int
+	if (check_textures(char *cub))
+		return (1);
+	if (check_map(char *cub))
+		return (1);
 
 
+
+	return (0);
+}
+
+int check_textures(char *cub)
+{
+	int		fd;
+	char	*line;
+
+	fd = open(cub, O_RDONLY);
+	if (fd < 0)
+		return (perror("Error"), 1);
+	line = get_next_line(fd);
+	while (line)
+	{
+
+		free(line);
+		line = get_next_line(line);
+	}
+		
+	return (0);
+}
+
+int check_map(char *cub)
+{
+	int	fd;
+
+	fd = open(cub, O_RDONLY);
+	if (fd < 0)
+		return (perror("Error"), 1);
+	
+	close(fd);	
 
 	return (0);
 }
