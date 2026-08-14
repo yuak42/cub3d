210406f12b3f3c967c6cdc798322f9e6a541c159
yunemak <yeak558@gmail.com>
2026-08-14 13:12:22 +0300

    fix: ft_strtrim error check on assign_texture function

--- project/src/parser/get_texture_paths_utils.c
+++ project/src/parser/get_texture_paths_utils.c
@@ -6,35 +6,47 @@
 /*   By: yuak <yuak@student.42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/06 12:43:58 by yuak              #+#    #+#             */
-/*   Updated: 2026/08/14 09:06:40 by yuak             ###   ########.fr       */
+/*   Updated: 2026/08/14 13:11:43 by yuak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+int	deal_texture(char **splitted, t_game *game);
+
 int assign_texture(t_game *game, char *line)
 {
 	char	**splitted;
+	char	*trimmed;
 
-	line = ft_strtrim(line, "\n"); // error check later
-	splitted = ft_split(line, ' ');
+	trimmed = ft_strtrim(line, "\n");
+	if (!line)
+		return (perror("Error"), 1);
+	splitted = ft_split(trimmed, ' ');
 	if (!splitted)
 		return (perror("Error"), 1);
-	if (!ft_strncmp(line, "NO ", 3))
-		game->texture.no = splitted[1];
-	else if (!ft_strncmp(line, "SO ", 3))
-		game->texture.so = splitted[1];
-	else if (!ft_strncmp(line, "WE ", 3))
-		game->texture.we = splitted[1];
-	else if (!ft_strncmp(line, "EA ", 3))
-		game->texture.ea = splitted[1];
-	else if (!ft_strncmp(line, "F ", 2))
-		assign_color(game, 'f', splitted[1]);
-	else if (!ft_strncmp(line, "C ", 2))
-		assign_color(game, 'c', splitted[1]);
+	if (deal_texture(splitted, game))
+		return (1);
 	free(splitted[0]);
 	free(splitted);
-	free(line); // free created line with ft_strtrim
+	free(trimmed);
+	return (0);
+}
+
+int	deal_texture(char **splitted, t_game *game)
+{
+	if (!ft_strncmp(splitted[0], "NO", 3))
+		game->texture.no = splitted[1];
+	else if (!ft_strncmp(splitted[0], "SO", 3))
+		game->texture.so = splitted[1];
+	else if (!ft_strncmp(splitted[0], "WE", 3))
+		game->texture.we = splitted[1];
+	else if (!ft_strncmp(splitted[0], "EA", 3))
+		game->texture.ea = splitted[1];
+	else if (!ft_strncmp(splitted[0], "F", 2))
+		assign_color(game, 'f', splitted[1]);
+	else if (!ft_strncmp(splitted[0], "C", 2))
+		assign_color(game, 'c', splitted[1]);
 	return (0);
 }
 
@@ -61,17 +73,17 @@ void	assign_color(t_game *game, char c, char *str)
 
 int is_texture_line(char *line)
 {
-	if (ft_strnstr(line, "NO ", 3))
+	if (!ft_strncmp(line, "NO ", 3))
 		return (1);
-	if (ft_strnstr(line, "SO ", 3))
+	if (!ft_strncmp(line, "SO ", 3))
 		return (1);
-	if (ft_strnstr(line, "WE ", 3))
+	if (!ft_strncmp(line, "WE ", 3))
 		return (1);
-	if (ft_strnstr(line, "EA ", 3))
+	if (!ft_strncmp(line, "EA ", 3))
 		return (1);
-	if (ft_strnstr(line, "F ", 2))
+	if (!ft_strncmp(line, "F ", 2))
 		return (1);
-	if (ft_strnstr(line, "C ", 2))
+	if (!ft_strncmp(line, "C ", 2))
 		return (1);
 	return (0);
 }
