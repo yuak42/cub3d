9ce4102e5928db451a4ba9470dc14135b2f2ad44
Baran Yaprak <baran.yaprak.by>
2026-08-16 22:02:23 +0000

    feat: Add set_position function to initialize player position based on map grid

---
+++ project/src/renderer/set_position.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_position.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: byaprak <byaprak@student.42istanbul.com.tr>  #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026-08-16 11:22:39 by byaprak           #+#    #+#             */
+/*   Updated: 2026-08-16 11:22:39 by byaprak          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+void	set_position(t_game *game)
+{
+	char	**grid;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	grid = game->map->grid;
+	while(grid[i])
+	{
+		j = 0;
+		while(grid[i][j])
+		{
+			if (grid[i][j]== 'N' || grid[i][j] == 'S' || grid[i][j]== 'E' || grid[i][j] == 'W')
+			{
+				game->player.x = j;
+				game->player.y = i;
+				return ;
+			}
+			j++;
+		}
+		i++;
+	}
+}
