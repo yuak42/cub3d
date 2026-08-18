eca2d83e92dafc0f5c22d75fd9eefe36a2673c00
Baran Yaprak <baran.yaprak.by>
2026-08-18 16:59:05 +0000

    feat: Add set_raydir function to calculate ray direction based on player position

---
+++ project/src/renderer/set_raydir.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   set_raydir.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: byaprak <byaprak@student.42istanbul.com.tr>  #+#  +:+       +#+      */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026-08-17 22:26:32 by byaprak           #+#    #+#             */
+/*   Updated: 2026-08-17 22:26:32 by byaprak          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+void	set_raydir(t_game *game, t_Rargs *args, int x)
+{
+	int	camerax;
+
+	camerax = ((2 * x) / game->map->width) - 1; // width ekran boyutu olacak. 
+	args->ray_dirx = game->player.dir_x + args->plane_x * camerax;
+	args->ray_diry = game->player.dir_y + args->plane_y * camerax;
+
+}
