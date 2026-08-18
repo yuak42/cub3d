16de0b7407b9090b077eb7f6bcbcc4e7e1330379
Baran Yaprak <baran.yaprak.by>
2026-08-18 16:59:24 +0000

    feat: Add get_spawn_dir function to determine player spawn direction from map grid

---
+++ project/src/renderer/utils.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: byaprak <byaprak@student.42istanbul.com.tr>  #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026-08-17 21:39:27 by byaprak           #+#    #+#             */
+/*   Updated: 2026-08-17 21:39:27 by byaprak          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+char get_spawn_dir(t_game *game)
+{
+	char	**grid;
+	char	*row;
+
+	grid = game->map->grid;
+	while(*grid)
+	{
+		row = *grid;
+		while(*row)
+		{
+			if (*row== 'N' || *row == 'S' || *row == 'E' || *row == 'W')
+				return (*row);
+			row++;
+		}
+		grid++;
+	}
+	return '0';
+}
