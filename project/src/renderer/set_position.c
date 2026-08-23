78794d5396323a774916ed09483481a563a8aa51
Baran Yaprak <baran.yaprak.by>
2026-08-23 16:05:34 +0000

    refactor: Simplify set_position function by removing unused grid logic

--- project/src/renderer/set_position.c
+++ project/src/renderer/set_position.c
@@ -14,26 +14,32 @@
 
 void	set_position(t_game *game)
 {
-	char	**grid;
-	int		i;
-	int		j;
-
-	i = 0;
-	j = 0;
-	grid = game->map->grid;
-	while(grid[i])
-	{
-		j = 0;
-		while(grid[i][j])
-		{
-			if (grid[i][j]== 'N' || grid[i][j] == 'S' || grid[i][j]== 'E' || grid[i][j] == 'W')
-			{
-				game->player.x = j+ 0.5;
-				game->player.y = i + 0.5;
-				return ;
-			}
-			j++;
-		}
-		i++;
-	}
+	game->player.x += 0.5;
+	game->player.y += 0.5;
 }
+
+// void	set_position(t_game *game)
+// {
+// 	char	**grid;
+// 	int		i;
+// 	int		j;
+
+// 	i = 0;
+// 	j = 0;
+// 	grid = game->map->grid;
+// 	while(grid[i])
+// 	{
+// 		j = 0;
+// 		while(grid[i][j])
+// 		{
+// 			if (grid[i][j]== 'N' || grid[i][j] == 'S' || grid[i][j]== 'E' || grid[i][j] == 'W')
+// 			{
+// 				game->player.x = j+ 0.5;
+// 				game->player.y = i + 0.5;
+// 				return ;
+// 			}
+// 			j++;
+// 		}
+// 		i++;
+// 	}
+// }
