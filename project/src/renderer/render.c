33013bc215d71c1e13fcc4f68ada32b5468ab681
Baran Yaprak <baran.yaprak.by>
2026-08-23 16:04:52 +0000

    refactor: Improve render function by initializing window and args properly

--- project/src/renderer/render.c
+++ project/src/renderer/render.c
@@ -15,15 +15,20 @@
 int	render(t_game *game)
 {
 	(void) game;
-	// t_Rargs *args;
+	t_render *args;
+	t_win	*window;
 
-	// args = malloc(sizeof(t_Rargs));
+	args = ft_calloc(sizeof(t_render), 1);
+	window = ft_calloc(sizeof(t_win), 1);
 	ft_printf("Game is being rendered\n");
 	// set_dir(game);
-	// set_position(game);
-	// set_plane(game, args);
-	// printf("dir_x:%f\ndir_y:%f\n", game->player.dir_x, game->player.dir_y);
-	// printf("x:%f\ny:%f\n", game->player.x, game->player.y);
-	// printf("plane_x:%f\nplane_y:%f\n", args->plane_x, args->plane_y);
+	if (!init_window(window))
+		return (0);
+	set_position(game);
+	set_plane(game, args);
+	set_raydir();
+	printf("dir_x:%f\ndir_y:%f\n", game->player.dir_x, game->player.dir_y);
+	printf("x:%f\ny:%f\n", game->player.x, game->player.y);
+	printf("plane_x:%f\nplane_y:%f\n", args->plane_x, args->plane_y);
 	return (0);
 }
