eae4e33daf5d28b41728417102a45655c8b20485
Baran Yaprak <baran.yaprak.by>
2026-08-24 18:10:04 +0000

    refactor: Move get_sidedist function definition and update set_map to call it

--- project/src/renderer/set_map.c
+++ project/src/renderer/set_map.c
@@ -12,6 +12,8 @@
 
 #include "cub3d.h"
 
+static void	get_sidedist(t_render *args, t_game *game);
+
 void	set_map(t_render *args, t_game *game)
 {
 	args->mapx = (int) (game->player.x);
@@ -24,6 +26,24 @@ void	set_map(t_render *args, t_game *game)
 		args->deltadisty = 1e30;
 	else
 		args->deltadisty = 1 / args->ray_diry;
+	get_stepx(args);
+	get_sidedist(args, game);
+}
+
+static void	get_sidedist(t_render *args, t_game *game)
+{
+	if (args->stepx == -1)
+		args->sidedistx = (game->player.x - args->mapx) * args->deltadistx;
+	else if (args->stepx == 1)
+		args->sidedistx = (game->player.x - args->mapx) * args->deltadistx;
+	if (args->stepy == -1)
+		args->sidedisty = (game->player.y - args->mapy) * args->deltadisty;
+	else if (args->stepy == 1)
+		args->sidedisty = (game->player.y - args->mapy) * args->deltadisty;
+}
+
+static void get_stepx(t_render *args)
+{
 	if (args->ray_dirx < 0)
 		args->stepx = -1;
 	else
