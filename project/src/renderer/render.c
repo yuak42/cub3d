b4305464221115e27f6f4b32edf8076ba92c8de7
Baran Yaprak <baran.yaprak.by>
2026-08-25 19:50:37 +0000

    refactor: Enhance render function to iterate over window width for raycasting

--- project/src/renderer/render.c
+++ project/src/renderer/render.c
@@ -17,21 +17,29 @@ int	render(t_game *game)
 	//(void) game;
 	t_render *args;
 	t_win	*window;
+	int	x;
 
+	x = 0;
 	args = ft_calloc(sizeof(t_render), 1);
 	window = ft_calloc(sizeof(t_win), 1);
 	ft_printf("Game is being rendered\n");
 	// set_dir(game);
 	if (!init_window(window))
 		return (0);
-	get_raycast_arg(game, args, window);
-	printf("dir_x:%f\ndir_y:%f\n", game->player.dir_x, game->player.dir_y);
-	printf("x:%f\ny:%f\n", game->player.x, game->player.y);
-	printf("plane_x:%f\nplane_y:%f\n", args->plane_x, args->plane_y);
-	printf("ray_dirx:%f\nray_diry:%f\n", args->ray_dirx, args->ray_diry);
-	printf("mapx:%d\nmapy:%d\n", args->mapx, args->mapy);
-	printf("deltadistx%f\ndeltadisty:%f\n", args->deltadistx, args->deltadisty);
-	printf("stepx:%d\nstepy:%d\n", args->stepx, args->stepy);
-	printf("sidedistx:%f\nsidedisty:%f\n", args->sidedistx, args->sidedisty);
+	// printf("dir_x:%f\ndir_y:%f\n", game->player.dir_x, game->player.dir_y);
+	// printf("x:%f\ny:%f\n", game->player.x, game->player.y);
+	// printf("plane_x:%f\nplane_y:%f\n", args->plane_x, args->plane_y);
+	// printf("ray_dirx:%f\nray_diry:%f\n", args->ray_dirx, args->ray_diry);
+	// printf("mapx:%d\nmapy:%d\n", args->mapx, args->mapy);
+	// printf("deltadistx%f\ndeltadisty:%f\n", args->deltadistx, args->deltadisty);
+	// printf("stepx:%d\nstepy:%d\n", args->stepx, args->stepy);
+	// printf("sidedistx:%f\nsidedisty:%f\n", args->sidedistx, args->sidedisty);
+	//printf("----------------width:%ld ********* height%ld--------\n", game->map->width, game->map->height);
+	while (x < window->w)
+	{
+		get_raycast_arg(game, args, window, x);
+		run_dda(args, game->map);
+		x++;
+	}
 	return (0);
 }
