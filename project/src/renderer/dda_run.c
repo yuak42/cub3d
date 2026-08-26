59361300e6eb6424d556ca4de9855d9dab1c16a3
Baran Yaprak <baran.yaprak.by>
2026-08-26 15:19:39 +0000

    Refactor: set size

--- project/src/renderer/dda_run.c
+++ project/src/renderer/dda_run.c
@@ -20,27 +20,29 @@ void	run_dda(t_render *args, t_map *map)
 	int	side;
 
 	hit = 0;
+	//printf("DDA Başlıyor - Işın yönü Y: %f, Başlangıç mapY: %d, stepY: %d\n", args->ray_diry, args->mapy, args->stepy);
 	while (hit == 0)
 	{
 		if (args->sidedistx < args->sidedisty)
 		{
 			args->sidedistx += args->deltadistx;
 			args->mapx +=args->stepx;
-			side = 0;
+			args->size.side = 0;
 		}
 		else
 		{
 			args->sidedisty += args->deltadisty;
 			args->mapy +=args->stepy;
-			side = 1;
+			args->size.side = 1;
 		}
+		//printf("  DDA Adımı - mapX: %d, mapY: %d\n", args->mapx, args->mapy);
 		if (!is_out_of_bounds(args, map))
 			break;
 		if (map->grid[args->mapy][args->mapx] == '1')
 			hit = 1;
 	}
-	if (hit == 1)
-		printf("map_x:%d map_y:%d side:%d\n", args->mapx, args->mapy, side);
+	// if (hit == 1)
+	// 	printf("map_x:%d map_y:%d side:%d\n", args->mapx, args->mapy, side);
 }
 
 static int is_out_of_bounds(t_render *args, t_map *map)
