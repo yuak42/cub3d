fa4bec02d4aca6786c885551f697d54dd131a6cb
yunemak <yeak558@gmail.com>
2026-08-28 20:18:39 +0300

    fix: Check map position

--- project/src/check_utils/is_map_position_true.c
+++ project/src/check_utils/is_map_position_true.c
@@ -6,7 +6,7 @@
 /*   By: yuak <yuak@student.42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/18 10:23:03 by yuak              #+#    #+#             */
-/*   Updated: 2026/08/26 17:49:38 by yuak             ###   ########.fr       */
+/*   Updated: 2026/08/28 20:17:53 by yuak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +22,25 @@ int	is_map_position_true(char *cub)
 	fd = open(cub, O_RDONLY);
 	if (fd < 0)
 		return (perror("Error"), 1000);
+	get_next_line(-1);
 	line = get_next_line(fd);
 	while (line)
 	{
-		if (is_map_line(line))
+		if (line[0] == '\n')
 		{
-			printf("The map is starting with line: %s", line);
+			free(line);
+			line = get_next_line(fd);
+			continue ;
+		}
+		else if (is_map_line(line))
+		{
 			if (go_end_of_map(fd))
-				return (close(fd), free(line), 1);
+				return (free(line), close(fd), print_error("Error\nMap position is wrong!"), 0);
 		}
 		free(line);
 		line = get_next_line(fd);
 	}
-	print_error("Error\nMap position is wrong!");
-	return (close(fd), 0);
+	return (close(fd), 1);
 }
 
 static int	go_end_of_map(int fd)
@@ -46,14 +51,18 @@ static int	go_end_of_map(int fd)
 	while (line)
 	{
 		if (!is_map_line(line))
-		{
-			printf("This line is not map line: %s", line);
-			return (free(line), 1);
-		}
-		printf("map line: %s", line);
+			break ;
 		free(line);
 		line = get_next_line(fd);
 	}
 	free(line);
-	return (0);
+	line = get_next_line(fd);
+	while (line && line[0] == '\n')
+	{
+		free(line);
+		line = get_next_line(fd);
+	}
+	if (!line)
+		return (0);
+	return (free(line), 1);
 }
