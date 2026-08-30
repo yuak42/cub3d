e822284af937d1632bfce3a53bb01609ed77957d
yunemak <yeak558@gmail.com>
2026-08-30 15:27:32 +0300

    fix: is_map_line first checks is it new line

--- project/src/check_utils/is_map_line.c
+++ project/src/check_utils/is_map_line.c
@@ -6,7 +6,7 @@
 /*   By: yuak <yuak@student.42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/26 13:18:37 by yuak              #+#    #+#             */
-/*   Updated: 2026/08/29 18:36:27 by yuak             ###   ########.fr       */
+/*   Updated: 2026/08/30 15:26:46 by yuak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int	is_map_line(char *line)
 {
+	if (*line == '\n')
+		return (0);
 	while (*line)
 	{
 		if (*line == ' ' || *line == '1' || *line == '0')
