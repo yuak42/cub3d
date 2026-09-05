77cab11bbd27eec0f7dc20659b0459fbe844bbb6
Baran Yaprak <baran.yaprak.by>
2026-09-05 22:31:20 +0000

    Fix: player direction calculate

--- project/src/renderer/set_position.c
+++ project/src/renderer/set_position.c
@@ -12,10 +12,12 @@
 
 #include "cub3d.h"
 
-void	set_position(t_game *game)
+void	set_position(t_game *game, t_render *args)
 {
 	game->player.x += 0.5;
 	game->player.y += 0.5;
+	args->dir_x = game->player.dir_x;
+	args->dir_y = game->player.dir_y;
 }
 
 // void	set_position(t_game *game)
