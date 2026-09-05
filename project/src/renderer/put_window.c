1a0107ed940be26cda30c6fc62e92cca8c842e84
Baran Yaprak <baran.yaprak.by>
2026-09-05 22:15:47 +0000

    Refactor put window

---
+++ project/src/renderer/put_window.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   put.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: byaprak <byaprak@student.42istanbul.com.tr>  #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026-09-04 23:52:25 by byaprak           #+#    #+#             */
+/*   Updated: 2026-09-04 23:52:25 by byaprak          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+int	put_game(t_render *args)
+{
+	int	x;
+	int	result;
+
+	x = 0;
+	while (x < args->window.w)
+	{
+		get_raycast_arg(args->game, args, x);
+		run_dda(args, args->game, x);
+		x++;
+	}
+	result = mlx_put_image_to_window(args->window.mlx_ptr, args->window.win_ptr, args->img.img_p, 0, 0);
+	return (result);
+}
