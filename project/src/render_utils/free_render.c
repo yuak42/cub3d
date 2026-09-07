0e63747d4a270523a0483b91049d27759aa8c730
Baran Yaprak <baran.yaprak.by>
2026-09-07 18:46:23 +0000

    feat: implement free_render to clean textures and game data

---
+++ project/src/render_utils/free_render.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_render.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: byaprak <byaprak@student.42istanbul.com.tr>  #+#  +:+       +#+      */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026-09-06 15:25:25 by byaprak           #+#    #+#             */
+/*   Updated: 2026-09-06 15:25:25 by byaprak          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+void	free_render(t_render *args)
+{
+	mlx_destroy_image(args->window.mlx_ptr, args->so.w_p);
+	mlx_destroy_image(args->window.mlx_ptr, args->no.w_p);
+	mlx_destroy_image(args->window.mlx_ptr, args->we.w_p);
+	mlx_destroy_image(args->window.mlx_ptr, args->ea.w_p);
+	free_game(args->game);
+	free(args);
+}
