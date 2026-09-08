a35c8cc2b629c14081b643b9f16e86e8a1471737
Baran Yaprak <baran.yaprak.by>
2026-09-08 12:20:13 +0000

    refactor: norm fix

--- project/src/render_utils/key_event.c
+++ project/src/render_utils/key_event.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   key_event.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: byaprak <byaprak@student.42istanbul.com.tr>  #+#  +:+       +#+        */
+/*   By: byaprak <byaprak@student.42istanbul.com.tr>  #+#  +:+       +#+      */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026-09-04 21:24:39 by byaprak           #+#    #+#             */
 /*   Updated: 2026-09-04 21:24:39 by byaprak          ###   ########.fr       */
@@ -15,7 +15,7 @@
 int	key_event(void *args)
 {
 	t_render	*ag;
-	int	keycode;
+	int			keycode;
 
 	ag = args;
 	keycode = ag->key_code;
@@ -23,11 +23,11 @@ int	key_event(void *args)
 	{
 		if (keycode == 65307)
 			close_win(args);
-		else if(keycode == 65361 || keycode == 65363)
+		else if (keycode == 65361 || keycode == 65363)
 			ray_move(args, ag->key_code);
-		else if(keycode == 119 || keycode == 97 || keycode == 115 || keycode == 100)
+		else if (keycode == 119 || keycode == 97
+			|| keycode == 115 || keycode == 100)
 			move_pose(args);
 	}
 	return (0);
 }
-
