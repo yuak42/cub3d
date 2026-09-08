e384bee40edb5e466c4cacd97f83fc6932f9a97b
Baran Yaprak <baran.yaprak.by>
2026-09-08 13:02:54 +0000

    refactor: norm fix

--- project/src/renderer/set_size.c
+++ project/src/renderer/set_size.c
@@ -12,43 +12,42 @@
 
 #include "cub3d.h"
 
-static void set_draw(t_render *args);
-static void set_wallx(t_render *args, t_game *game);
+static void	set_draw(t_render *args);
+static void	set_wallx(t_render *args, t_game *game);
 static void	set_tex_x(t_render *args);
-static void tex_x_mirror(t_render *args, int texwidth);
+static void	tex_x_mirror(t_render *args, int texwidth);
 
-void set_wall_size(t_render *args, t_game *game)
+void	set_wall_size(t_render *args, t_game *game)
 {
 	if (args->size.side == 0)
 		args->size.perpwalldist = args->sidedistx - args->deltadistx;
-	else if(args->size.side == 1)
+	else if (args->size.side == 1)
 		args->size.perpwalldist = args->sidedisty - args->deltadisty;
 	args->size.lineheight = (int)(args->window.h / args->size.perpwalldist);
-	//printf("perpwalldist:%f\nlineheight:%d\n", args->size.perpwalldist, args->size.lineheight);
 	set_draw(args);
 	set_wallx(args, game);
 	set_tex_x(args);
 }
 
-static void set_draw(t_render *args)
+static void	set_draw(t_render *args)
 {
 	args->size.drawstart = -args->size.lineheight / 2 + args->window.h / 2;
 	if (args->size.drawstart < 0)
 		args->size.drawstart = 0;
-	args->size.drawend = args->size.lineheight / 2 + args->window.h  / 2;
+	args->size.drawend = args->size.lineheight / 2 + args->window.h / 2;
 	if (args->size.drawend > args->window.h - 1)
 		args->size.drawend = args->window.h - 1;
-	//printf("draw_end:%d\ndraw_start:%d\n", args->size.drawend, args->size.drawstart);
 }
 
-static void set_wallx(t_render *args, t_game *game)
+static void	set_wallx(t_render *args, t_game *game)
 {
 	if (args->size.side == 0)
-		args->size.wall_x = game->player.y + args->size.perpwalldist * args->ray_diry;
+		args->size.wall_x = game->player.y
+			+ args->size.perpwalldist * args->ray_diry;
 	else if (args->size.side == 1)
-		args->size.wall_x = game->player.x + args->size.perpwalldist * args->ray_dirx;
+		args->size.wall_x = game->player.x
+			+ args->size.perpwalldist * args->ray_dirx;
 	args->size.wall_x -= floor(args->size.wall_x);
-	//printf("wall_x:%f\n", args->size.wall_x);
 }
 
 static void	set_tex_x(t_render *args)
@@ -69,10 +68,9 @@ static void	set_tex_x(t_render *args)
 	}
 	args->size.tex_x = (int)(args->size.wall_x * args->wall.tex_w);
 	tex_x_mirror(args, args->wall.tex_w);
-	//printf("tex_x:%d\n", args->size.tex_x);
 }
 
-static void tex_x_mirror(t_render *args, int texwidth)
+static void	tex_x_mirror(t_render *args, int texwidth)
 {
 	if (args->size.side == 0)
 	{
