4b663a33f01cd94e30cfe4b45a85dd77d9c96d4a
yunemak <yeak558@gmail.com>
2026-09-08 16:17:58 +0300

    feat: Adapt is_there_map

--- project/src/check_utils/is_there_map.c
+++ project/src/check_utils/is_there_map.c
@@ -6,7 +6,7 @@
 /*   By: yuak <yuak@student.42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/24 10:05:08 by yuak              #+#    #+#             */
-/*   Updated: 2026/08/30 15:42:08 by yuak             ###   ########.fr       */
+/*   Updated: 2026/09/08 16:16:16 by yuak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,15 @@ int	is_there_map(char *cub)
 	fd = open(cub, O_RDONLY);
 	if (fd < 0)
 		return (perror("Error"), 1);
-	line = get_next_line(fd);
+	if (read_next_line(fd, &line))
+		return (perror("Error"), close(fd), read_next_line(-1, NULL), 0);
 	while (line)
 	{
 		if (is_map_line(line))
-			return (get_next_line(-1), free(line), close(fd), 1);
+			return (free(line), close(fd), read_next_line(-1, NULL), 1);
 		free(line);
-		line = get_next_line(fd);
+		if (read_next_line(fd, &line))
+			return (perror("Error"), close(fd), read_next_line(-1, NULL), 0);
 	}
 	print_error("Error\nThere is no map\n");
 	close(fd);
--- project/src/free_utils/free_game.c
+++ project/src/free_utils/free_game.c
@@ -6,7 +6,7 @@
 /*   By: yuak <yuak@student.42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/13 08:24:00 by yuak              #+#    #+#             */
-/*   Updated: 2026/08/26 17:45:43 by yuak             ###   ########.fr       */
+/*   Updated: 2026/09/08 16:13:32 by yuak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,5 @@ void	free_game(t_game *game)
 	free_map(game->map);
 	free(game);
 	get_next_line(-1);
+	read_next_line(-1, NULL);
 }
