445b0e2350e7b347c62d6b2292f09af01d867bd5
Baran Yaprak <baran.yaprak.by>
2026-09-08 12:20:38 +0000

    refactor: norm fix

--- project/src/renderer/dda_run.c
+++ project/src/renderer/dda_run.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   dda_run.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: byaprak <byaprak@student.42istanbul.com.tr>  #+#  +:+       +#+        */
+/*   By: byaprak <byaprak@student.42istanbul.com.tr>  #+#  +:+       +#+      */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026-08-25 01:04:17 by byaprak           #+#    #+#             */
 /*   Updated: 2026-08-25 01:04:17 by byaprak          ###   ########.fr       */
@@ -12,45 +12,44 @@
 
 #include "cub3d.h"
 
-static int is_out_of_bounds(t_render *args, t_map *map);
+static int	is_out_of_bounds(t_render *args, t_map *map);
+static void	put_texture(t_render *args, t_game *game, int x);
 
 void	run_dda(t_render *args, t_game *game, int x)
 {
 	int	hit;
 
 	hit = 0;
-	//printf("DDA Başlıyor - Işın yönü Y: %f, Başlangıç mapY: %d, stepY: %d\n", args->ray_diry, args->mapy, args->stepy);
 	while (hit == 0)
 	{
 		if (args->sidedistx < args->sidedisty)
 		{
 			args->sidedistx += args->deltadistx;
-			args->mapx +=args->stepx;
+			args->mapx += args->stepx;
 			args->size.side = 0;
 		}
 		else
 		{
 			args->sidedisty += args->deltadisty;
-			args->mapy +=args->stepy;
+			args->mapy += args->stepy;
 			args->size.side = 1;
 		}
-		//printf("  DDA Adımı - mapX: %d, mapY: %d\n", args->mapx, args->mapy);
 		if (!is_out_of_bounds(args, game->map))
-			break;
+			break ;
 		if (game->map->grid[args->mapy][args->mapx] == '1')
 			hit = 1;
 	}
 	if (hit == 1)
-	{
-		set_wall_size(args, game);
-		put_window(args, game, x);
-		//printf("***********************************************")
-	}
-
-	// 	printf("map_x:%d map_y:%d side:%d\n", args->mapx, args->mapy, side);
+		put_texture(args, game, x);
 }
 
-static int is_out_of_bounds(t_render *args, t_map *map)
+static void	put_texture(t_render *args, t_game *game, int x)
+{
+	set_wall_size(args, game);
+	put_window(args, game, x);
+}
+
+static int	is_out_of_bounds(t_render *args, t_map *map)
 {
 	if (args->mapx >= 0 && args->mapx < (int) map->width)
 	{
