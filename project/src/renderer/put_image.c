2317301aa4b7d07d45c3f360f2ec4d87caf28487
Baran Yaprak <baran.yaprak.by>
2026-09-09 20:53:17 +0000

    fix; floor and ceiling get color fix

--- project/src/renderer/put_image.c
+++ project/src/renderer/put_image.c
@@ -57,8 +57,16 @@ static void	set_rgb(t_render *args, t_game *game, int x, int y)
 {
 	int	color;
 
-	color = game->texture.c.r << 16
+	if (y < args->size.drawstart)
+	{
+		color = game->texture.c.r << 16
 		| game->texture.c.g << 8 | game->texture.c.b;
+	}
+	if (y > args->size.drawstart)
+	{
+		color = game->texture.f.r << 16
+		| game->texture.f.g << 8 | game->texture.f.b;
+	}
 	image_piksel_put(args, color, x, y);
 }
 
