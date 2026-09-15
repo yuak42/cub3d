ddbee0c151b25b98ba8f088802647aee6011364a
yunemak <yeak558@gmail.com>
2026-09-15 20:32:52 +0300

    Delete debug lines and unused cub3d.h

--- project/src/main.c
+++ project/src/main.c
@@ -6,17 +6,14 @@
 /*   By: yuak <yuak@student.42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/15 20:37:38 by yuak              #+#    #+#             */
-/*   Updated: 2026/09/15 16:19:22 by yuak             ###   ########.fr       */
+/*   Updated: 2026/09/15 20:31:37 by yuak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "cub3d.h"
 
 #include "check.h"
 #include "free.h"
 #include "parser.h"
-
-static void	print_result(t_game *game);
+#include "render.h"
 
 int	main(int ac, char **av)
 {
@@ -29,17 +26,8 @@ int	main(int ac, char **av)
 		return (2);
 	if (check_map(game))
 		return (free_game(game), 3);
-	print_result(game);
 	if (render(game))
 		return (free_game(game), 4);
 	free_game(game);
 	return (0);
-}
-
-static void	print_result(t_game *game)
-{
-	printf("Game was parsed successfully\n");
-	print_map(game->map);
-	print_texture_paths(game);
-	print_player_info(game);
 }
