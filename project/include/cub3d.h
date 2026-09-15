41fe7c4c9f7c7e5f1152db9bdef7e2259a745bca
yunemak <yeak558@gmail.com>
2026-09-15 14:40:26 +0300

    fix: Byaprak update

--- project/include/cub3d.h
+++ project/include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: yuak <yuak@student.42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/26 20:00:21 by yuak              #+#    #+#             */
-/*   Updated: 2026/09/15 10:44:57 by yuak             ###   ########.fr       */
+/*   Updated: 2026/09/15 14:39:34 by yuak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,36 +25,15 @@
 #include <math.h>
 
 #include "free.h" // delete this later
+#include "render.h" // delete
 
 
 // Render
-int		render(t_game *game);
 
 // Delete later
 void 	print_map(t_map *map);
 void	print_texture_paths(t_game *game);
 void	print_player_info(t_game *game);
 void 	print_grid(char **grid);
-
-void	set_dir(t_game *game);
-void	set_position(t_game *game, t_render *args);
-char	get_spawn_dir(t_game *game);
-void	set_plane(t_game *game, t_render *args);
-void	set_raydir(t_render *args, double x);
-int		init_window(t_render *args);
-void	set_map(t_render *args, t_game *game);
-void	get_raycast_arg(t_game *game, t_render *args, int x);
-void	run_dda(t_render *args, t_game *game, int x);
-void	set_wall_size(t_render *args, t_game *game);
-int		put_window(t_render *args, t_game *game, int x);
-int		key_event(void *args);
-void	ray_move(t_render *args);
-int		close_win(t_render *args);
-int		put_game(t_render *args);
-int		key_release(int key_press, void *args);
-int		key_press(int key_press, void *args);
-void	move_pose(t_render *args);
-void	free_render(t_render *args);
-int		is_walkable(t_game *game, double x, double y);
 
 #endif
---
+++ project/include/render.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yuak <yuak@student.42istanbul.com.tr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/15 14:37:15 by yuak              #+#    #+#             */
+/*   Updated: 2026/09/15 14:39:36 by yuak             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RENDER_H
+# define RENDER_H
+
+int		render(t_game *game);
+void	set_dir(t_game *game);
+void	set_position(t_game *game, t_render *args);
+char	get_spawn_dir(t_game *game);
+void	set_plane(t_game *game, t_render *args);
+void	set_raydir(t_render *args, double x);
+int		init_window(t_render *args);
+void	set_map(t_render *args, t_game *game);
+void	get_raycast_arg(t_game *game, t_render *args, int x);
+void	run_dda(t_render *args, t_game *game, int x);
+void	set_wall_size(t_render *args, t_game *game);
+int		put_window(t_render *args, t_game *game, int x);
+int		key_event(void *args);
+void	ray_move(t_render *args);
+int		close_win(t_render *args, int status);
+int		put_game(t_render *args);
+int		key_release(int key_press, void *args);
+int		key_press(int key_press, void *args);
+void	move_pose(t_render *args);
+void	free_render(t_render *args);
+int		is_walkable(t_game *game, double x, double y);
+
+#endif
